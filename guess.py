a34d7631240d13c83b96925b6eb0aa895f49a660
nvansturgill <vanburen.nia@gmail.com>
2020-08-11 17:59:38 -0400

    Add `guess.py` to root 	- Named as `guess` not `myGuessingGame` so I can test quickly in 	  python IDE

---
+++ guess.py
@@ -0,0 +1,7 @@
+
+# Import built-in module for generating (random) number
+import random
+
+# Prompt, store, and print user name as `user`
+user = input('Hello, there. Enter your name to begin... ')
+print("Welcome, {}!".format(user))
