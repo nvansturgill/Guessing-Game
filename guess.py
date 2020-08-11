c8447899cadc9592286b93eac63338a3c8f19401
nvansturgill <vanburen.nia@gmail.com>
2020-08-11 19:20:10 -0400

    Add MVP `guess.py`

--- guess.py
+++ guess.py
@@ -3,5 +3,23 @@
 import random
 
 # Prompt, store, and print user name as `user`
-user = input('Hello, there. Enter your name to begin... ')
-print("Welcome, {}!".format(user))
+user = input("Hello, there. Enter your name to begin... ")
+print("Alright, {}!".format(user))
+print("I'm thinking of a number between 1 and 10")
+
+# Generate random integer and store as `num`
+number = random.randint(1, 10)
+# Initialize `number_of_guesses` for counting tries (guesses)
+number_of_guesses = 0
+
+while number_of_guesses < 10:
+    guess = int(input("Take a guess: "))
+    number_of_guesses += 1
+    if guess < number:
+        print("Shucks, your number is too low.")
+    if guess > number:
+        print("Rats! Your number is too high.")
+    if guess == number:
+        print("You guessed the number!")
+        print("{} gets a gold star." .format(user))
+        break
